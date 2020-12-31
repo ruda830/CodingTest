9dfb7d1b3f8183b9d002f3604038f817a19e1835
ruda <sheimisky7150@yahoo.co.jp>
2020-12-31 10:30:56 +0900

    class hogehogeの使い方の勉強

---
+++ study/class.py
@@ -0,0 +1,26 @@
+#classの勉強
+
+#クラス定義
+class TestClass:
+    pass
+
+#利用したい場合、関数の実行と同じように()付けてインスタンス生成
+x = TestClass()
+
+#同じ名前で再定義すると上書きされるっぽい
+class TestClass:
+    x = "変数1"
+
+    #クラス内の関数はメゾットと呼ぶ
+    #第一引数にそのクラスのインスタンスを表すオブジェクト、selfを受け取る
+    def test_method1(self):
+        print(self.x)
+
+    def test_method2(self, arg1):
+        print("引数1:" + arg1)
+
+
+testClass = TestClass()
+testClass.test_method1()
+#testClass.test_method2()←これだとmissing required argumentと出る。
+testClass.test_method2("引数Test")
