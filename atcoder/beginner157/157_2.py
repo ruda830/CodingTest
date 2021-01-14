027d4304a5198f19f3ba5ebc5a9ceb6b03dcff1f
ruda <sheimisky7150@yahoo.co.jp>
2021-01-14 16:40:42 +0900

    or条件にはDPで解決した。これでいいのかわからないけど、どの条件でも今のところ大丈夫だった。うれしい。

--- atcoder/beginner157/157_2.py
+++ atcoder/beginner157/157_2.py
@@ -1,51 +1,71 @@
 """
-3×3のサイズのビンゴカードが、最終的にビンゴしているかどうかを判定する問題です。
+N＊Nのサイズのビンゴカードが、最終的にビンゴしているかどうかを判定する問題です。
 入力
 3
 oox
 xoo
 oxo
 
-oox
-xoo
-ooo
-
+出力
 Yes
 """
-import sys
-
+#入力
 N = int(input()) # 入力回数をNとして受け取る
-list = [input() for i in range(N)]
-#print(list) ->['oxox', 'xxox', 'ooxo', 'oooo']
-cnt = 0
-if __name__ == '__main__':
-    alllist = []
-    for j in range(N):
-        alllist += list[j]
+list = [input() for i in range(N)] #print(list) ->['oxox', 'xxox', 'ooxo', 'oooo']
+#斜め、横判定用のリスト
+alllist = []
+for j in range(N):
+    alllist += list[j]
 
+
+#Yes判定
+try:
+    for i in range(N): #もしtryの上にこれ書くと一つずつYesNo判定されてしまう。
+        if list[i].count('o') == N: #横判定
+            print('Yes')
+            break
+
+        elif alllist[i::N+1].count('o') == N: #斜め判定
+            print('Yes')
+            break
+
+        elif alllist[i::N].count('o') == N: #縦判定
+            print('Yes')
+            break
+
+
+#No判定
+finally:
+#横判定→斜め判定→縦判定。どれか当てはまったら'Yes'でbreak,当てはまらなかった順から下↓の判定。
+#tryの条件と同じにしないと、’Yes'の時に’No’も発動してしまう。でも、かなり変な気がする。
+
+    if list[i].count('o') != N: #必須。なんでiがこの位置にあるのに正確に動くのか分からない。
+        #print('No_1')
+        if alllist[i::N + 1].count('o') != N:
+            #print('No_2')
+            if alllist[i::N].count('o') != N:
+                print('No_3')
+
+
+#ここからメモ------------------------------
+"""
+    if list[i].count('x') >= 1:
+        print('debag')
+    でもいい。
+"""
+"""
     for k in range(N):
         if alllist[k::N+1].count('o') == N: #ifの位置ここでいいのかわからん。
             print('Yes')
             break
         else:
             print('debag')
-
-try:
-    for i in range(N): #もしtryの上にこれ書くと一つずつYesNo判定されてしまう。
-        if list[i].count('o') == N:
-            print('Yes')
-            break
-finally:
-    if list[i].count('o') != N: #必須。なんでiがこの位置にあるのに正確に動くのか分からない。
-        print('No')
-
-    # list[i].count('x') >= 1:
-    #   print('debag')
-    #でもいい。
-
+"""
+#a = [[str(c) for c in l.strip()] for l in sys.stdin]
 
 
 """
+入力場合分け
 4
 oxox
 xxox
@@ -58,15 +78,28 @@ xoox
 oxoo
 ooxo
 
-
+4
 oxox
 xxox
 ooxo
 oooo
-
+4
 oooo
 oxox
 xxox
 ooxo
-#a = [[str(c) for c in l.strip()] for l in sys.stdin]
+
+4
+ooxo
+oxox
+xxox
+oooo
+
+4
+oooo
+oxox
+xxox
+oooo
+
+
 """
